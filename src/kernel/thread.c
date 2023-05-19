d168f066180d341f9eecdc084cf4ef4bd7c829a5
sherlok682 <321268235@qq.com>
2023-05-19 15:54:47 +0800

    device_request

--- src/kernel/thread.c
+++ src/kernel/thread.c
@@ -58,11 +58,10 @@ void init_thread()
 void test_thread()
 {
     set_interrupt_state(true);
-    u32 counter = 0;
-
-    while(true)
+    test();
+    LOGK("test finished of task %d\n", getpid());
+    while (true)
     {
-        test();
-        // sleep(2000);
+        sleep(10);
     }
 }
